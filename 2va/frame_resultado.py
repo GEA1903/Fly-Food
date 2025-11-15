620eb147d4afd7d1e924b1ed9ae6c5234e4f4d40
Gabriel Aversa Escobar <gea190307@gmail.com>
2025-11-15 10:54:15 -0300

    Enhance Resultado class with execution time and close option
    Added execution time display and close button.

--- 2va/frame_resultado.py
+++ 2va/frame_resultado.py
@@ -3,27 +3,32 @@ from main import FoodDelivery
 
 
 class Resultado(ctk.CTkFrame):
-    def __init__ (self, master, matriz, rota, distancia,metodo):
+    def __init__ (self, master, matriz, rota, distancia,metodo, tempo):
         super().__init__(master)
         self.pack(fill='both', expand=True)
         
-        ctk.CTkLabel(self, text='Resultado da melhor rota').pack(pady=10)
+        ctk.CTkLabel(self, text='Resultado da melhor rota', font=('Arial', 18, 'bold')).pack(pady=(40, 20))
 
-        ctk.CTkLabel(self, text=f'Método usado:{metodo}').pack(pady=5)
-        ctk.CTkLabel(self, text=f'Distância total: {distancia} dronômetros').pack(pady=5)
+        ctk.CTkLabel(self, text=f'Método usado: {metodo}').pack(pady=10)
+        ctk.CTkLabel(self, text=f'Distância total: {distancia} dronômetros').pack(pady=10)
+        ctk.CTkLabel(self, text=f'Tempo de execução: {tempo:.2f} segundos').pack(pady=5)
 
         #Exibição da matriz calculada
 
-        ctk.CTkLabel(self, text="Matriz analisada:").pack(pady=10)
+        ctk.CTkLabel(self, text="Matriz analisada:").pack(pady=(20, 10))
         caixa=ctk.CTkTextbox(self, height=150)
         caixa.insert('1.0', matriz)
         caixa.configure(state='disabled')
         caixa.pack(pady=10)
 
         #Botão para voltar
-        ctk.CTkButton(self, text='Voltar', command=self.voltar).pack(pady=15, side='right')
+        ctk.CTkButton(self, text='Fechar', command=self.fechar_programa).pack(pady=15, padx=10, side='right')
+        ctk.CTkButton(self, text='Voltar', command=self.voltar).pack(pady=15,padx=10, side='right')
+        
 
     def voltar(self):
         self.master.mostrar_inicial()
+    def fechar_programa(self):
+        self.master.destroy()
 
         
